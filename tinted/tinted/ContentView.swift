f6d99c6565d9b50a72dae5968beecb9a4cd07724
divyasuri06 <divyas06@vt.edu>
2026-05-17 18:00:47 -0700

    initial: Tinted Xcode project setup

---
+++ tinted/tinted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tinted
+//
+//  Created by divya suri  on 5/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tinted/tintedTests/tintedTests.swift
@@ -0,0 +1,18 @@
+//
+//  tintedTests.swift
+//  tintedTests
+//
+//  Created by divya suri  on 5/17/26.
+//
+
+import Testing
+
+struct tintedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
